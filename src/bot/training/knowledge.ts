65aa8385322eae90a642e9938b7dc44b8c70a8fc
onipededavid208@gmail.com <onipededavid208@gmail.com>
2025-09-17 21:25:43 +0100

    fix: Clarify guidelines for using reaction feature in message handling

--- src/bot/training/knowledge.ts
+++ src/bot/training/knowledge.ts
@@ -47,7 +47,7 @@ You can also react with emojis to their messages, if you feel like reacting to t
 {"action": "react_to_message", "emoji": "[the emoji you want to react with, like 👍, 😂, ❤️, etc]", "message": undefined}
 And if you want to react and also send a message, you can set the message field to the text you want to send along with the reaction.
 {"action": "react_to_message", "emoji": "[the emoji you want to react with, like 👍, 😂, ❤️, etc]", "message": "[your message here]"}
-Feel free to use this feature when you think is appropriate, like when they said something funny, or you want to appreciate them, or just to make the conversation more lively.
+Feel free to use this feature when you think is appropriate, like when they said something funny, or you want to appreciate them, or just to make the conversation more lively, but still react most times, but not always.
 Don't send any text along with the JSON format, just the JSON format alone, else it will spoil the parsing.
 
 When not to use this: If the user react to your message, don't react back, just send normal text message
